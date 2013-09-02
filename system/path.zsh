0d1b4ddd4ccb4c7a111b461221eb9b755a058892
travmik <travmik@gmail.com>
2013-09-02 11:32:51 +0300

    Fixing zsh settings

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="/bin:$HOME/env/mysql/bin:$HOME/env/maven/bin:$HOME/env/ant/bin:$HOME/env/jre/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="/bin:$HOME/env/mysql/bin:$HOME/env/maven/bin:$HOME/env/ant/bin:$HOME/env/jre/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
